514138310cf9b88ce775c11acc007fbfea8da359
Andy Puertas <andy.puertas08@gmail.com>
2018-06-13 12:01:39 -0600

    events now show up with buttons (still not functional)

--- src/Component/CalendarNav/CalendarNav.js
+++ src/Component/CalendarNav/CalendarNav.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 import Event from '../Event/Event';
+import './CalendarNav.css';
 
 export default class CalendarNav extends Component {
     constructor(props){
@@ -8,15 +9,41 @@ export default class CalendarNav extends Component {
         this.state = {
             events: []
         }
+        this.getEvents = this.getEvents.bind( this )
+    }
+
+    componentDidMount() {
+        this.getEvents()
+    }
+
+    getEvents() {
+        axios.get('/api/events')
+        .then((res) => {
+            this.setState({
+                events: res.data
+            })
+        })
     }
 
     
 
     render() {
+        //console.log(this.state)
+        let events = this.state.events.map( element => {
+            return(
+                <div>
+                    <Event event={element} key={element.id}/>
+                </div>
+            )
+        }
+    )
+        
+        
         return(
             <div>
-                <h3>EVENT CALENDAR</h3>
-                <Event />
+                <h2>EVENT CALENDAR</h2>
+                {/* <Event /> */}
+                {events}
             </div>
         )
     }
--- src/Component/Event/Event.js
+++ src/Component/Event/Event.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-export default function Events(props){
+export default function Event (props){
+    let {artist, date, time, price, img} = props.event;
+    console.log(props)
         return(
-            <div>Event</div>
+            <div>
+                <img src={img} alt='logo' />
+                <p>{artist}</p>
+                <p>{date}</p> 
+                <p>Show: {time}</p>
+                <p>${price}</p>
+                <button>FIND TICKETS NOW</button>
+                <button>EVENT DETAILS</button>
+                <br />
+                <br />
+
+            </div>
         )
 }
+                

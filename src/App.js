5138a91407c7ad9a54e859bdfbe326bf4b2054df
Andy Puertas <andy.puertas08@gmail.com>
2018-06-28 12:27:30 -0600

    trying to get reset.css to be recognized

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './Component/Header/Header';
 import Footer from './Component/Footer/Footer';
 import routes from './routes';
 import './App.css';
-import '/Users/andypuertas/Desktop/webdev/devmountain/wpr37/personal-project/depot-clone/src/reset.css'
+import '../src/reset.css';
 
 class App extends Component {
   render() {
